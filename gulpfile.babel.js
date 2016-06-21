d848601a626f15a36e496997dbf6b097ca0dd274
matsuo@TAM <matsuo@tam-tam.co.jp>
2016-06-21 10:57:22 +0900

    add watchify, uglify, sourcemaps

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,46 @@
 import gulp from 'gulp';
 import browserify from 'browserify';
 import babelify from 'babelify';
+import watchify from 'watchify';
+import buffer from 'vinyl-buffer';
 import source from 'vinyl-source-stream';
+import gulpLoadPlugins from 'gulp-load-plugins';
+const $ = gulpLoadPlugins();
+
+function bundle(watching = false) {
+  // `src/scripts/main.js` を `assets/scripts.js` にビルド
+  const b = browserify({
+    entries: ['src/scripts/main.js'],
+    transform: ['babelify'],
+    debug: true,
+    plugin: (watching) ? [watchify] : null
+  })
+  .on('update', () => {
+    bundler();
+    console.log('scripts rebuild');
+  });
+
+  function bundler() {
+    return b.bundle()
+      .on('error', (err) => {
+        console.log(err.message);
+      })
+      .pipe(source('scripts.js'))
+      .pipe(buffer())
+      .pipe($.sourcemaps.init({loadMaps: true}))
+      .pipe($.uglify())
+      .pipe($.sourcemaps.write('./'))
+      .pipe(gulp.dest('assets/'));
+  }
+
+  return bundler();
+}
 
 gulp.task('build', () => {
-  // `src/scripts/main.js` を `assets/scripts.js` にビルド
-  browserify({
-    entries: ['src/scripts/main.js'],
-    transform: ['babelify']
-  })
-  .bundle()
-  .pipe(source('scripts.js'))
-  .pipe(gulp.dest('assets/'));
+  bundle();
+});
+
+gulp.task('watch', () => {
+  bundle(true);
+  // その他の watch タスクがあればここに書く (CSSなど)
 });
